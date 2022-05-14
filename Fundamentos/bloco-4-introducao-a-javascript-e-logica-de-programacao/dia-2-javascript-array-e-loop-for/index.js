8e2b4d96a0f7ca333ed67f75bc36f7e8a38f826c
Leticia Oliveira <oliveira01e@hotmail.com>
2022-05-14 15:32:36 -0300

    exercicio 3

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/index.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/index.js
@@ -7,10 +7,19 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 //2.Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
 
+//let soma = 0;
+
+//for (let index = 0; index < numbers.length; index += 1) {
+//    soma += numbers[index]
+//}
+ //   console.log(soma);
+
+
+ //3. Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+
 let soma = 0;
 
 for (let index = 0; index < numbers.length; index += 1) {
     soma += numbers[index]
 }
-    console.log(soma);
-
+    console.log(soma / 10);
