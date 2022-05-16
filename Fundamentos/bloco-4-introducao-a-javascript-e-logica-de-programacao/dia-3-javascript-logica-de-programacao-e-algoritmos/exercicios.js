2366892f70686c129d6b28e9f716858ad46a28a5
Leticia Oliveira <oliveira01e@hotmail.com>
2022-05-16 18:59:41 -0300

    exercicio 2

--- Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
@@ -1,9 +1,19 @@
-//O fatorial é representado pelo sinal !
+//1. O fatorial é representado pelo sinal !
 //! = 4 x 3 x 2 x 1 = 24
 
-let fatorial = 10;
+//let fatorial = 10;
 
-for(let index = 1; index < 10; index += 1){
-    fatorial = fatorial * index
+//for(let index = 1; index < 10; index += 1){
+ //   fatorial = fatorial * index
+//}
+//console.log(fatorial)
+
+//2.
+
+let word = 'tryber';
+contra = " ";
+
+for(let index = 0; index < word.length; index += 1){
+   contra += word[word.length -1 -index]
 }
-console.log(fatorial)
+console.log(contra)
